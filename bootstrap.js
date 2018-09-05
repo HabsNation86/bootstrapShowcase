7a8a1bb83136108259c1d20a97cc99c187583e26
Jamie <jamiemckeown469@gmail.com>
2018-09-05 17:43:03 -0400

    isolated buttons

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,16 @@
 console.log(" jQuery");
 
-$("h1").on("click", function(){
+// Isolated french button
+$("#frenchButton").on("click", function(){
   console.log("test1");
 });
 
-$("h2").on("click", function(){
+// Isolated english button
+$("#englishButton").on("click", function(){
   console.log("test2");
 });
 
+// Isolated "Entreprise Loyal"
+$(".navbar-brand").on("click", function(){
+  console.log("test3");
+});
